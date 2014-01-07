69aeb64ca0c6db514c94af0415dd4fffeb2f7d62
Melinda Mizuta <mkmizuta@gmail.com>
2014-01-06 21:30:39 -0800

    change route for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Meldoku::Application.routes.draw do
 
-  resources :games
-  post "games/create"
+  resources :games, path: ''
+  # post "games/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
