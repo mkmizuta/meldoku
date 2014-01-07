080c938e973beba6a04ce4f46027d4bbc1508b11
Melinda Mizuta <mkmizuta@gmail.com>
2014-01-06 21:41:33 -0800

    updated routes for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Meldoku::Application.routes.draw do
 
-  resources :games, path: ''
-  # post "games/create"
+  resources :games
+  post "games/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
